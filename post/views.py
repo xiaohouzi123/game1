cb01c6d377338d3457ee8dee9d2cb6ef003e8f4f
Seamile <lanhuermao@gmail.com>
2018-06-04 11:33:49 +0800

    添加了文章处理功能

--- post/views.py
+++ post/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from post.models import Post
 
 
 def create(request):
-    return render(request, 'create.html', {})
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        return render(request, 'create.html')
 
 
 def edit(request):
-    return render(request, 'edit.html', {})
+    if request.method == 'POST':
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(id=post_id)
+
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(id=post_id)
+        return render(request, 'edit.html', {'post': post})
 
 
 def read(request):
-    return render(request, 'read.html', {})
+    post_id = int(request.GET.get('post_id'))
+    try:
+        post = Post.objects.get(id=post_id)
+        return render(request, 'read.html', {'post': post})
+    except Post.DoesNotExist:
+        return redirect('/')
 
 
 def post_list(request):
@@ -18,4 +42,6 @@ def post_list(request):
 
 
 def search(request):
-    return render(request, 'search.html', {})
+    keyword = request.POST.get('keyword')
+    posts = Post.objects.filter(content__contains=keyword)
+    return render(request, 'search.html', {'posts': posts})
