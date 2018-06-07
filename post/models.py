853d24793a652de66e8128686f7c1e8ae5645228
Seamile <lanhuermao@gmail.com>
2018-06-07 16:00:40 +0800

    添加了 Tag 功能

--- post/models.py
+++ post/models.py
@@ -18,6 +18,27 @@ class Post(models.Model):
     def comments(self):
         return Comment.objects.filter(post_id=self.id).order_by('-created')
 
+    def tags(self):
+        relations = PostTagRelation.objects.filter(post_id=self.id).only('tag_id')
+        tag_id_list = [r.tag_id for r in relations]
+        return Tag.objects.filter(id__in=tag_id_list)
+
+    def update_tags(self, tag_names):
+        '''更新帖子与标签的关系'''
+        tag_names = set(tag_names)
+        Tag.ensure_tag_name(tag_names)  # 确保传入的 tag name 在 Tag 表中存在
+
+        old_tags = self.tags()
+        old_tag_names = set(t.name for t in old_tags)
+
+        # 创建与新 Tags 的关系
+        need_add_relation_tag_names = tag_names - old_tag_names
+        PostTagRelation.add_post_tags(self.id, need_add_relation_tag_names)
+
+        # 删除不存在的关系
+        need_del_relation_tag_names = old_tag_names - tag_names
+        PostTagRelation.del_post_tags(self.id, need_del_relation_tag_names)
+
 
 class Comment(models.Model):
     uid = models.IntegerField()
@@ -36,3 +57,64 @@ class Comment(models.Model):
         if not hasattr(self, '_post'):
             self._post = Post.objects.get(id=self.post_id)
         return self._post
+
+
+class Tag(models.Model):
+    '''标签'''
+    name = models.CharField(max_length=16)
+
+    def posts(self):
+        relations = PostTagRelation.objects.filter(tag_id=self.id).only('post_id')
+        post_id_list = [r.post_id for r in relations]
+        return Post.objects.filter(id__in=post_id_list)
+
+    @classmethod
+    def ensure_tag_name(cls, tag_names):
+        '''确保 tag_names 存在，不存在的创建出来'''
+        exist_tags = cls.objects.filter(name__in=tag_names)
+        exist_tag_names = [t.name for t in exist_tags]
+        need_create_names = [name for name in tag_names if name not in exist_tag_names]
+        if need_create_names:
+            need_create_tags = [cls(name=name) for name in need_create_names]
+            cls.objects.bulk_create(need_create_tags)
+
+
+class PostTagRelation(models.Model):
+    '''
+        文章              标签
+
+        念奴娇.id         宋词.id
+        念奴娇.id         古典.id
+        念奴娇.id         文学.id
+        巡山.id          西游记.id
+        巡山.id          古典.id
+        巡山.id          文学.id
+        巡山.id          神话.id
+    '''
+    post_id = models.IntegerField()
+    tag_id = models.IntegerField()
+
+    @classmethod
+    def add_relation(cls, post_id, tag_id):
+        '''添加 post 与 tag 的关系'''
+        cls.objects.create(post_id=post_id, tag_id=tag_id)
+
+    @classmethod
+    def del_relation(cls, post_id, tag_id):
+        '''删除 post 与 tag 的关系'''
+        cls.objects.get(post_id=post_id, tag_id=tag_id).delete()
+
+    @classmethod
+    def add_post_tags(cls, post_id, tag_names):
+        '''批量添加 post 与 tag 的关系'''
+        tags = Tag.objects.filter(name__in=tag_names).only('id')
+        tag_id_list = [t.id for t in tags]
+        need_create_relations = [cls(post_id=post_id, tag_id=tid) for tid in tag_id_list]
+        cls.objects.bulk_create(need_create_relations)
+
+    @classmethod
+    def del_post_tags(cls, post_id, tag_names):
+        '''批量删除 post 与 tag 的关系'''
+        tags = Tag.objects.filter(name__in=tag_names).only('id')
+        tag_id_list = [t.id for t in tags]
+        cls.objects.filter(post_id=post_id, tag_id__in=tag_id_list).delete()
--- post/views.py
+++ post/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 
 from post.models import Post
 from post.models import Comment
+from post.models import Tag
 from post.helper import page_cache
 from post.helper import read_count
 from post.helper import get_top_n
@@ -31,11 +32,18 @@ def edit(request):
         post.title = request.POST.get('title')
         post.content = request.POST.get('content')
         post.save()
+
+        tags = request.POST.get('tags', '')
+        tag_names = [w.strip().title()
+                     for w in tags.replace('，', ',').split(',')
+                     if w.strip()]
+        post.update_tags(tag_names)
         return redirect('/post/read/?post_id=%d' % post.id)
     else:
         post_id = int(request.GET.get('post_id'))
         post = Post.objects.get(id=post_id)
-        return render(request, 'edit.html', {'post': post})
+        tag_str = ', '.join([t.name for t in post.tags()])
+        return render(request, 'edit.html', {'post': post, 'tags': tag_str})
 
 
 @read_count
@@ -88,3 +96,9 @@ def comment(request):
     content = request.POST.get('content')
     Comment.objects.create(uid=uid, post_id=post_id, content=content)
     return redirect('/post/read/?post_id=%s' % post_id)
+
+
+def tag_filter(request):
+    tag_id = int(request.GET.get('tag_id'))
+    tag = Tag.objects.get(id=tag_id)
+    return render(request, 'tag_filter.html', {'posts': tag.posts()})
